f9858ccd1457e29cbff41c76c0ca4caa4a292436
pilyeooong <pilyeooong@gmail.com>
2024-08-20 15:19:05 +0900

    add. redis

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,4 +68,12 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.cache_store = :redis_cache_store, {
+    url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/0" },
+    connect_timeout: 30,
+    read_timeout: 1,
+    write_timeout: 1,
+    reconnect_attempts: 1,
+  }
 end
