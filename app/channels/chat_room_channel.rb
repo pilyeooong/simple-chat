4f0b6913845f24dff98ae07098f48d10cde445ab
pilyeooong <pilyeooong@gmail.com>
2024-08-23 13:02:14 +0900

    fix. to use channel policy method

--- app/channels/chat_room_channel.rb
+++ app/channels/chat_room_channel.rb
@@ -2,9 +2,9 @@ class ChatRoomChannel < ApplicationCable::Channel
   def subscribed
     chat_room_id = params[:chat_room_id]
     chat_room = ChatRoom.find_by(id: chat_room_id)
-    reject_subscription if chat_room.nil?
+    reject if chat_room.nil?
 
-    stream_from "chat_room_#{chat_room.id.to_s}"
+    stream_from ChannelPolicy.chat_room_channel(chat_room.id.to_s)
   end
 
   def unsubscribed
--- app/channels/chat_room_list_channel.rb
+++ app/channels/chat_room_list_channel.rb
@@ -1,6 +1,6 @@
 class ChatRoomListChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "chat_room_list"
+    stream_from ChannelPolicy.chat_room_list_channel
   end
 
   def unsubscribed
